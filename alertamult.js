025671e9b7613e45aa284001689d82ba79d71161
Alexandra <anyta822@gmail.com>
2025-08-09 15:05:29 -0500

    html

---
+++ alertamult.js
@@ -0,0 +1,5 @@
+let mult1 = 20;
+var mult2 = 3;
+let resultado = mult1 * mult2;
+console.log("El resultado de la multiplicacion es" + resultado);
+alert(resultado);
---
+++ textoanumero.js
@@ -0,0 +1,8 @@
+var num1 = prompt("digite un numero");
+var num2 = prompt("digite otro numero");
+ num1 = parseInt(num1);
+ num2 = parseInt(num2);
+var suma = num1+num2;
+console.log(suma);
+alert("La suma de la operacion es" + suma);
+
