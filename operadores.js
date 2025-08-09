36ce5660f8960b2e384b7fc38f1cb88fdc6054cc
Alexandra <anyta822@gmail.com>
2025-08-09 09:59:50 -0500

    clase09082025

---
+++ operadores.js
@@ -0,0 +1,36 @@
+//suma,resta, multiplicacion y division
+//calcular valores de dos o mas variables
+let num1 = 4;
+var num2 = 8;
+let suma = num1+num2;
+console.log("el resultado es"  +  suma);
+//contadores
+let edad =24;
+edad =edad + 1
+edad ++;
+console.log("La edad es"+edad);
+//resta
+let rest1 =12;
+let rest2 =8;
+let resta =rest1-rest2;
+console.log("el resultado de la resta es" + resta);
+//contadores resta
+let kilos =100;
+kilos--;
+console.log("el numero de kilos es" + kilos);
+//multiplicacion
+let multi1 =10;
+let multi2 =50;
+let multiplicacion =multi1*multi2;
+console.log("el resultado de la multiplicacion es" + multiplicacion);
+//division
+let divis1 =50;
+let divis2 =2;
+let division =divis1/divis2;
+console.log("el division es" + division);
+//residuo
+let resi1 =25;
+let resi2 =4;
+let residuo =resi1%resi2;
+console.log("el residuo de la operacion es" +residuo);
+
